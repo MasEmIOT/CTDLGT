c4a3ffb96c4fcf8bbf16a1a042203daf7dc5c7cc
MasEmIOT <datducthuong@gmail.com>
2024-04-03 09:51:42 +0700

    Add file

---
+++ BaiTapC2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int array[n];
+    for(int i = 0; i < n; i++){
+        cin >> array[i];
+    }
+
+    for(int i  = 0; i < n; i++){
+        for(int j = i+1; j < n; j++){
+            if(array[i] > array[j]){
+                swap(array[i],array[j]);
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
